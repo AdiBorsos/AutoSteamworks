439fca4e8e4629dc26018e4c2bffad1028f6f2d7
Cooper <cooper.davies@vizworx.com>
2020-05-14 00:15:06 -0600

    In the process of rewriting SaveData. This is by no means a criticism of the original code. I am simply re-organizing it in a way that I think might be more user-freindly to a future developer.

--- AutoSteamApp/OldSaveData.cs
+++ AutoSteamApp/OldSaveData.cs
@@ -5,10 +5,20 @@ using AutoSteamApp.Core;
 
 namespace AutoSteamApp
 {
-    public class SaveData
+    public class OldSaveData
     {
+
+        /// <summary>
+        /// The MHW Process
+        /// </summary>
         private Process _mhw = null;
+
+        /// <summary>
+        /// The Start address to the Save Data of a player. Offset_to_Base + Offset_to_SaveData
+        /// </summary>
         private ulong StartAddress = Settings.Off_Base + Settings.Off_SaveData;
+
+        // Pointer to where the 
         private ulong offset1;
         private ulong offset2;
 
@@ -17,8 +27,10 @@ namespace AutoSteamApp
         public short SteamGauge => MemoryHelper.Read<short>(_mhw, offset2 + 0x102FE4);
         public byte BonusTime => MemoryHelper.Read<byte>(_mhw, offset2 + 0x102FE6);
 
-        public SaveData(Process mhw, CancellationTokenSource cts)
+
+        public OldSaveData(Process mhw, CancellationTokenSource cts)
         {
+            // Set the proper process for the save data object
             _mhw = mhw;
 
             PlayerData.Load(_mhw, cts);
@@ -30,12 +42,18 @@ namespace AutoSteamApp
 
     public static class PlayerData
     {
+
+        /// <summary>
+        /// Address of the user's save data (slot independent). This is the offset of the base game plus the offset to the save data
+        /// </summary>
         private static ulong StartAddress = Settings.Off_Base + Settings.Off_SaveData;
 
         public static int CurrentSlot { get; private set; } = -1;
 
         public static void Load(Process mhw, CancellationTokenSource cts)
         {
+
+            // Get the pointer found 0xA8 bytes into the start address
             var offset1 = MemoryHelper.Read<ulong>(mhw, StartAddress) + 0xA8;
 
             int[] slotPlayTimes = new int[3] { -1, -1, -1 };
---
+++ AutoSteamApp/Process Memory/SaveData.cs
@@ -0,0 +1,120 @@
+﻿using AutoSteamApp.Core;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AutoSteamApp.Process_Memory
+{
+    public class SaveData
+    {
+
+        /// <summary>
+        /// The Monster Hunter World: Iceborne process to load player data from.
+        /// </summary>
+        Process MHWProcess;
+
+        /// <summary>
+        /// The cancellation token used for invoking a cnacellation of the program.
+        /// </summary>
+        CancellationTokenSource CancellationToken;
+
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor used to generate player data from.
+        /// </summary>
+        /// <param name="mhwProcess">The Monster Hunter World: Iceborne process to load player data from.</param>
+        /// <param name="cancellationToken">The cancellation token used for invoking a cnacellation of the program.</param>
+        /// <param name="Pointer1Value">The value found in the po</param>
+        public SaveData(Process mhwProcess, CancellationTokenSource cancellationToken)
+        {
+            MHWProcess = mhwProcess;
+            CancellationToken = cancellationToken;
+            LoadData();
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Loads/Reloads the Save Data of the current MHW Process
+        /// </summary>
+        public void LoadData()
+        {
+            /* First we need to find out which save slot the player is currently using
+             *  We cleverly check this by looking for the "Play Time" value of each slot after a certain time.
+             * The slot whose play time increased is the slot we are looking for. This may take a few tries as
+             */
+
+            //First we find the address pointed to by the save data pointer
+            ulong SaveDataAddress = MemoryHelper.Read<ulong>(MHWProcess, MHWMemoryValues.SaveDataPointer);
+            int slotNumber = GetSlotNumber(SaveDataAddress);
+            
+
+        }
+
+
+        int GetSlotNumber(ulong SaveDataAddress)
+        {
+            // We know that the pointer to the slot data is X bytes after the save data pointer value
+            ulong SlotDataPointer = SaveDataAddress + MHWMemoryValues.OffsetToSlotDataPointer;
+
+            int[] SlotPlayTimes = new int[3] { -1, -1, -1 };
+
+            //We loop through each slot to set the initial playtime. 
+            for (int slotNumber = 0; slotNumber < 3; slotNumber++)
+            {
+                // Grab the slot data address from the slot data pointer
+                ulong SlotDataAddress = MemoryHelper.Read<ulong>(MHWProcess, SlotDataPointer);
+
+                // We need to go to the correct slot though which is the slot size * slot number
+                SlotDataAddress += MHWMemoryValues.SlotSize * (ulong)slotNumber;
+
+                // The pointer is x bytes into the slot
+                ulong PlayTimePointer = SlotDataAddress + MHWMemoryValues.OffsetToPlayTimePointer;
+
+                // Get the playtime from the playtime pointer
+                SlotPlayTimes[slotNumber] = MemoryHelper.Read<int>(MHWProcess, PlayTimePointer);
+            }
+
+            // I have explicitly duplicated the above code to make it more clear to a reader, rather than
+            // Doing it in a more "programmy" way.
+            int retVal = -1;
+            while (!CancellationToken.IsCancellationRequested)
+            {
+                // Repeat the same as above until one of the slots changes or cancellation is requested. 
+                // This indicates that the changed slot is being played currently.
+                //We loop through each slot to set the initial playtime. 
+                for (int slotNumber = 0; slotNumber < 3; slotNumber++)
+                {
+                    // Grab the slot data address from the slot data pointer
+                    ulong SlotDataAddress = MemoryHelper.Read<ulong>(MHWProcess, SlotDataPointer);
+
+                    // We need to go to the correct slot though which is the slot size * slot number
+                    SlotDataAddress += MHWMemoryValues.SlotSize * (ulong)slotNumber;
+
+                    // The pointer is x bytes into the slot
+                    ulong PlayTimePointer = SlotDataAddress + MHWMemoryValues.OffsetToPlayTimePointer;
+
+                    // If the playtime has been updated, we have identified which slot the player is using
+                    if (MemoryHelper.Read<int>(MHWProcess, PlayTimePointer) != SlotPlayTimes[slotNumber])
+                    {
+                        retVal = slotNumber;
+                        break;
+                    }
+                }
+            }
+            return retVal;
+            
+        }
+
+        #endregion
+
+    }
+}
