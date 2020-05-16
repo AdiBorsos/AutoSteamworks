5e404d17094f3c5050c4abc5201005cc449c3a34
Cooper <cooper.davies@vizworx.com>
2020-05-16 00:54:14 -0600

    added class for retrieving the mhw process (from Adi's code)

---
+++ AutoSteamApp/Helpers/StaticHelpers.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoSteamApp.Helpers
+{
+    public static class StaticHelpers
+    {
+        /// <summary>
+        /// Returns the process which contains the process name as defined in the fields
+        /// </summary>
+        /// <returns></returns>
+        public static Process GetMHWProcess()
+        {
+            //Retrieve all processes with defined process name
+            var processes = Process.GetProcessesByName(AutomatonConfiguration.ProcessName);
+            try
+            {
+                // Try to return the first one
+                return processes.FirstOrDefault(p => p != null && p.ProcessName.Equals(AutomatonConfiguration.ProcessName) && !p.HasExited);
+            }
+            catch
+            {
+                throw new Exception("Could not find a running MHW process.");
+            }
+        }
+    }
+}
