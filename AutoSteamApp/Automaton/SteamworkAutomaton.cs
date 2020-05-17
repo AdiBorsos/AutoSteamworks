5775ec0a5bcd78f9e05fefc23ba090071b0a48c3
Cooper <cooper.davies@vizworx.com>
2020-05-17 00:21:09 -0600

    implemented load and verify of the mhw process

--- AutoSteamApp/Automaton/SteamworkAutomaton.cs
+++ AutoSteamApp/Automaton/SteamworkAutomaton.cs
@@ -5,6 +5,7 @@ using Logging;
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -14,20 +15,29 @@ namespace AutoSteamApp
     public class SteamworkAutomaton
     {
 
-        // First thing is to get the MHWProcess
+        /// <summary>
+        /// Process loaded in the constructor.
+        /// </summary>
         Process mhwProcess;
 
-        // Using the process, load the save data;
+        /// <summary>
+        /// Save data loaded in the constructor.
+        /// </summary>
         SaveData saveData;
 
+        /// <summary>
+        /// Field used to flag whether or not the currently running MHW:IB version is supported.
+        /// </summary>
+        bool SupportedVersion = false;
+
+        /// <summary>
+        /// Constructor which loads all required process memory and configuration values.
+        /// </summary>
         public SteamworkAutomaton()
         {
             try
             {
-                // First set the mhw process
-                mhwProcess = StaticHelpers.GetMHWProcess();
-
-
+                LoadAndVerifyProcess();
             }
             catch (Exception e)
             {
@@ -37,15 +47,41 @@ namespace AutoSteamApp
             }
         }
 
-
-
-
-
-
         public void Run(CancellationToken cts)
         {
 
             Environment.Exit(1);
         }
+
+        #region Helpers
+
+        /// <summary>
+        /// Loads and verifies the current MHW:IB process against the currently supported version.
+        /// </summary>
+        /// TODO: Make it more oo by making this the constructor for an abstract automaton.
+        /// Then have inhertied automatons work based on different versions.
+        /// This would allow for backward compatability
+        /// 
+        private void LoadAndVerifyProcess()
+        {
+            // First set the mhw process
+            mhwProcess = StaticHelpers.GetMHWProcess();
+
+            // Now verify the version
+            Match match = Regex.Match(mhwProcess.MainWindowTitle, AutomatonConfiguration.SupportedVersionRegex);
+            // If the match is made
+            if (match.Success)
+                // And we have a capture group
+                if (match.Groups.Count > 1)
+                    // Try to turn it into a number
+                    if (int.TryParse(match.Groups[1].Value, out int result))
+                        // If it is a numeber and is the same as the supported version
+                        if (result == AutomatonConfiguration.SupportedGameVersion)
+                            // Set the flag
+                            SupportedVersion = true;
+        }
+
+        #endregion
+
     }
 }
