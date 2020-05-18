4bccb33f8f4b79c7e4130b39ca11a40427e6d26e
Cooper <ctdavies@ucalgary.ca>
2020-05-18 14:04:27 -0600

    started coding the read sequence method

--- AutoSteamApp/Process Memory/SteamworksData.cs
+++ AutoSteamApp/Process Memory/SteamworksData.cs
@@ -1,4 +1,8 @@
 ﻿using AutoSteamApp.Core;
+using AutoSteamApp.Helpers;
+using GregsStack.InputSimulatorStandard.Native;
+using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 
 namespace AutoSteamApp.Process_Memory
@@ -73,6 +77,27 @@ namespace AutoSteamApp.Process_Memory
             PhaseAddress = SteamworksAddress + MHWMemoryValues.OffsetToSteamPhase;
             // Offset to find the rarity
             RarityAddress = SteamworksAddress + MHWMemoryValues.OffsetToGameRarity;
+        }
+
+        public Queue<VirtualKeyCode> ExtractSequence()
+        {
+            byte[] bytes = new byte[3];
+            bytes[0] = MemoryHelper.Read<byte>(MHWProcess, SequenceAddress);
+            bytes[1] = MemoryHelper.Read<byte>(MHWProcess, SequenceAddress + 1);
+            bytes[2] = MemoryHelper.Read<byte>(MHWProcess, SequenceAddress + 2);
+
+            // return null if they are are all zero (indicates the minigame hasnt started)
+            if (bytes.AllIdentical())
+                if (bytes[0] == 0)
+                    return null;
+                else
+                    throw new Exception("Error reading correct sequence. An unknown sequence has been encountered. This is most likely due to an incompatible game version");
+
+            //Capcom has likely done some manipulation so we need to do some twidling because the sequences aren't as straightforward as we'd like.
+
+
+
+            throw new NotImplementedException();
         }
 
         #endregion
